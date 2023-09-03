0e2babbd9635555a0fcc9a314b93c98968b757ae
ravvnen <phillip.boe.jensen@gmail.com>
2023-09-03 10:06:18 +0200

    change name, and update, to handle comments

--- parser_1.py
+++ parser_1.py
@@ -2,37 +2,31 @@ import os
 import re
 
 regex_comment = r'(?:/\*(?:[^*]|(?:\*+[^*/]))*\*+/)|(?://.*)'
-regex_comment_test = r'//.*'
 regex_import = r'import\s+([a-zA-Z0-9_.]+);'
 regex_import_solo = r'(?<=import\s)[a-zA-Z0-9_.*]+;'
 
 
-def remove_comments(file_path):
-    with open(file_path, 'r') as file:
-        content = file.readlines()
-        for line in content:
-            match = re.search(regex_comment_test, line)
-            if match:
-                print(f"File: {file_path}, Import: {match.group()}")
+def remove_comments(text):
+    return re.sub(regex_comment, '', text)
 
 
 def find_imports(file_path):
     with open(file_path, 'r') as file:
-        content = file.readlines()
-        for line in content:
-            match = re.search(regex_import_solo, line)
+        content = file.read()
+        content_cleaned = remove_comments(content)
+        for match in re.finditer(regex_import_solo, content_cleaned):
             if match:
                 print(f"File: {file_path}, Import: {match.group()}")
 
 
 def main():
     # Replace with your folder path
-    folder_path = '/Users/phillipravn/course-02242-examples'
+    folder_path = 'C:\\Users\\phill\\GitHub\\Project-Analysis'
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.endswith('.java'):
                 file_path = os.path.join(root, file)
-                # remove_comments(file_path)
+                remove_comments(file_path)
                 find_imports(file_path)
 
 
