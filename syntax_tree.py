d75605062a7c2abcd611a97a1a54ffc3cb7053e0
ravvnen <phillip.boe.jensen@gmail.com>
2023-09-07 11:55:37 +0200

    latest update

--- syntax_tree.py
+++ syntax_tree.py
@@ -1,5 +1,9 @@
+
 from tree_sitter import Language, Parser
-import graphviz as pgv
+import pygraphviz as pgv
+import os
+
+folder_path = 'C:\\Users\\phill\\GitHub\\Project-Analysis'
 
 FILE = "./languages.so"  # the ./ is important
 Language.build_library(FILE, ["tree-sitter-java"])
@@ -11,25 +15,43 @@ with open("simple/Example.java", "rb") as f:
     tree = parser.parse(f.read())
 # the tree is now ready for analysing
 # print(tree.root_node.sexp())
-
 G = pgv.AGraph(strict=False, directed=True)
 
 
 def traverse(node, parent_class=None):
+
     if node.type == 'class_declaration':
-        class_name = node.children[0].value
-        print(parent_class)
+        class_name = node.children[1].text
         G.add_node(class_name)
 
     if node.type == 'method_declaration':
-        method_name = node.children[1].value
+        method_name = node.children[2].text
+        G.add_node(method_name)
+        G.add_edge(parent_class, method_name, label="has method")
 
     if node.type == 'field_declaration':
-        field_name = node.children[1].value
+        field_name = node.children[1].text
+        G.add_node(field_name)
+        G.add_edge(parent_class, field_name, label="has field")
 
     for child in node.children:
         traverse(child, parent_class=class_name if node.type ==
                  'class_declaration' else parent_class)
 
 
-traverse(tree.root_node)
+# traverse(tree.root_node, parent_class=None)
+# G.draw("imgettingpizzatonight.png", prog="dot")
+
+
+def main():
+    G = pgv.AGraph(strict=False, directed=True)
+
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            # checking only java files
+            if file.endswith('.java'):
+                file_path = os.path.join(root, file)
+                traverse(tree.root_node, parent_class=None)
+
+    # the visualization
+    G.draw("imgettingpizzatonight.png", prog="dot")
